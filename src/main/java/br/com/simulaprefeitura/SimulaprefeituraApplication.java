848ef78bf29c0e72ae6851e659d60b29788bf815
Gustavo Henrique Canuto Cruz <131610213+GustavoCanuto@users.noreply.github.com>
2023-10-22 19:48:13 -0300

    teste

--- src/main/java/br/com/simulaprefeitura/SimulaprefeituraApplication.java
+++ src/main/java/br/com/simulaprefeitura/SimulaprefeituraApplication.java
@@ -2,8 +2,10 @@ package br.com.simulaprefeitura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SimulaprefeituraApplication {
 
 	public static void main(String[] args) {
