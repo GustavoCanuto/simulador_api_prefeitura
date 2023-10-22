b1c9928f7239c79300094221c6f946a8926ceb6d
Gustavo Henrique Canuto Cruz <131610213+GustavoCanuto@users.noreply.github.com>
2023-10-22 20:17:48 -0300

    mais um

--- src/main/java/br/com/simulaprefeitura/SimulaprefeituraApplication.java
+++ src/main/java/br/com/simulaprefeitura/SimulaprefeituraApplication.java
@@ -2,7 +2,6 @@ package br.com.simulaprefeitura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
